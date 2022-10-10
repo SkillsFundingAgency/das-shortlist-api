50f188c810137800da62f6a6c249343f072142fc
Deven Shah <devenshah1974@gmail.com>
2022-10-10 11:26:22 +0100

    CSP-270 Added database project

---
+++ src/SFA.DAS.Shortlist.Database/Tables/Shortlist.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Shortlist]
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [ShortlistUserId] UNIQUEIDENTIFIER NOT NULL,
+    [UkPrn] INT NOT NULL,
+    [StandardId] INT NOT NULL,
+    [LocationDescription] VARCHAR(1000) NULL,
+    [Lat] FLOAT NULL,
+    [Long] FLOAT NULL,
+    [CreatedDate] DATETIME NOT NULL DEFAULT GETDATE(),
+    CONSTRAINT PK_Shortlist PRIMARY KEY ([Id]) 
+)
+GO
+
+CREATE NONCLUSTERED INDEX [IDX_Shortlist_UserItems] ON [dbo].[Shortlist] (ShortlistUserId) WITH (ONLINE=ON)
+GO
