033cabf6b133121d2dbb40bd44cecaa323c60ced
Deven Shah <devenshah1974@gmail.com>
2022-10-10 11:25:59 +0100

    CSP-270 Added API project

---
+++ src/SFA.DAS.Shortlist.Api/Program.cs
@@ -0,0 +1,42 @@
+using NLog.Web;
+
+namespace SFA.DAS.Shortlist.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            NLogBuilder.ConfigureNLog("nlog.config");
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Host.UseNLog();
+            builder.Services.AddHealthChecks();
+
+            var app = builder.Build();
+
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapHealthChecks("/ping");
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
